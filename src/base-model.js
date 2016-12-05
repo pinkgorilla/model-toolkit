84f16a58a56a2f158dc136be791093c334e26898
Tris Setiawan <tris.setiawan@yahoo.com>
2016-12-05 12:03:18 +0000

    fix reviver

--- src/base-model.js
+++ src/base-model.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const dateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/;
+const dateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z$/;
 module.exports = class BaseModel {
     constructor(type, version) {
         this._stamp = "";
